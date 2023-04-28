8cc7a8ed57b4a6a38b727cf1f26050c1dba18442
asachko <zaratuthtra@gmail.com>
2023-04-28 10:25:02 +0000

    version added

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "m1t1/home/asachko/programming/DO_course/M2T5/telebot/cmd"
+import "github.com/asachko/telebot/cmd"
 
 func main() {
 	cmd.Execute()
